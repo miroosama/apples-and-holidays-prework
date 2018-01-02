6727788ae164384d49d7dc25139aae0fa301071e
osama miro <o.miro650@gmail.com>
2018-01-02 21:25:31 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -19,7 +19,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 holiday_hash[season][holiday_name] = supply_array
 holiday_hash
 end
-end
+
 
 def all_winter_holiday_supplies(holiday_hash)
 
