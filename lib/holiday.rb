0a3396507c87a9c03988fd2e6bf4ba4918d2d696
osama miro <o.miro650@gmail.com>
2018-01-02 21:21:30 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -12,7 +12,11 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-
+holiday_hash.each do |key, value|
+  if key["memorial day"]
+    value << supply
+  end
+end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
